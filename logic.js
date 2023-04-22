b94616117f4c459906b8c0efd33cc6d2ed6a1240
Max <55982815+TheDemon2179@users.noreply.github.com>
2023-04-22 17:44:48 +0300

    [release]
    ugh

---
+++ logic.js
@@ -0,0 +1,45 @@
+function countSpecialCharacters(text) {
+    var specialCharacters = [
+      '!', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/',
+      ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'
+    ];
+    var count = 0;
+    for (var i = 0; i < text.length; i++) {
+      if (specialCharacters.indexOf(text[i]) !== -1) {
+        count++;
+      }
+    }
+    return count;
+}
+
+function count() {
+    var text = document.getElementById("text").value;
+    var characterCount = text.length;
+    
+    var wordCount = text.split(" ").length;
+    var specialCharacterCount = countSpecialCharacters(text);
+
+    document.getElementById("character-count").innerHTML = characterCount;
+    document.getElementById("wildcard-count").innerHTML = wildcardCount;
+    document.getElementById("word-count").innerHTML = wordCount;
+    document.getElementById("special-character-count").innerHTML = specialCharacterCount;
+}
+  
+  document.getElementById("text").addEventListener("keyup", count);
+  
+  function copyToClipboard() {
+    var text = document.getElementById("text").value;
+    var dummy = document.createElement("textarea");
+    document.body.appendChild(dummy);
+    dummy.value = text;
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+  }
+  
+  function clearText() {
+    var confirm = confirm("Are you sure you want to clear the text?");
+    if (confirm) {
+      document.getElementById("text").value = "";
+    }
+  }
